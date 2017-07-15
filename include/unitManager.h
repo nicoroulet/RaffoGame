43ab37b5db5b04599afb986dc3ba04aba40a2e99
Gaston Fourcade <gfourcade@dc.uba.ar>
2017-07-15 00:03:58 -0300

    tabs -> spaces, in every file

--- include/unitManager.h
+++ include/unitManager.h
@@ -15,36 +15,36 @@
 
 class unitManager {
 private:
-	struct compare {
-		bool operator() (Unit* u1, Unit* u2) const {return *u1 < *u2;}
-	};
-	sorted_array<Unit*, compare> units;
-	std::set<Unit*> selected;
+    struct compare {
+        bool operator() (Unit* u1, Unit* u2) const {return *u1 < *u2;}
+    };
+    sorted_array<Unit*, compare> units;
+    std::set<Unit*> selected;
 
-	int display_width;
-	int display_height;
-	int last_click_x;
-	int last_click_y;
-	bool clicked;
-	drawable_rectangle rect;
+    int display_width;
+    int display_height;
+    int last_click_x;
+    int last_click_y;
+    bool clicked;
+    drawable_rectangle rect;
 
 public:
-	unitManager(int maxPop);
-	template <class UnitType>
-	void create(int x, int y);
-	void right_unclick(int x, int y, bool shift);
-	void left_click(int x, int y);
-	void left_unclick(int x, int y, bool shift);
-	void mouse_move(int x, int y);
-	void tick();
+    unitManager(int maxPop);
+    template <class UnitType>
+    void create(int x, int y);
+    void right_unclick(int x, int y, bool shift);
+    void left_click(int x, int y);
+    void left_unclick(int x, int y, bool shift);
+    void mouse_move(int x, int y);
+    void tick();
 };
 
 template <class UnitType>
 void unitManager::create(int x, int y) {
-	static_assert(std::is_base_of<Unit, UnitType>::value,
-		"Type parameter of this class must derive from Unit");
-	// FIXME: delete this new or change the way of creating units (smart poiners?)
-	UnitType* u = new UnitType();
-	u->set_position(x,y);
-	units.insert(u);
+    static_assert(std::is_base_of<Unit, UnitType>::value,
+        "Type parameter of this class must derive from Unit");
+    // FIXME: delete this new or change the way of creating units (smart poiners?)
+    UnitType* u = new UnitType();
+    u->set_position(x,y);
+    units.insert(u);
 }
--- src/unitManager.cpp
+++ src/unitManager.cpp
@@ -11,85 +11,85 @@ using namespace std;
 
 
 unitManager::unitManager(int maxPop) :
-	units(maxPop),
-	clicked(false) {}
+    units(maxPop),
+    clicked(false) {}
 
 void unitManager::right_unclick(int x, int y, bool shift) {
-	for (auto it = selected.begin(); it != selected.end(); ++it) {
-		cerr << "moving\n";
-		(*it)->move(x,y);
-	}
+    for (auto it = selected.begin(); it != selected.end(); ++it) {
+        cerr << "moving\n";
+        (*it)->move(x,y);
+    }
 }
 
 void unitManager::left_click(int x, int y) {
-	last_click_x = x;
-	last_click_y = y;
-	rect.set_values(y, y, x, x);
-	clicked = true;
+    last_click_x = x;
+    last_click_y = y;
+    rect.set_values(y, y, x, x);
+    clicked = true;
 }
 
 void unitManager::left_unclick(int x, int y, bool shift) {
-	rect.clear();
-	clicked = false;
+    rect.clear();
+    clicked = false;
 
-	if (!shift) {
-		for (auto it = selected.begin(); it != selected.end(); ++it)
-			(*it)->unselect();
-		selected.clear();
-	}
+    if (!shift) {
+        for (auto it = selected.begin(); it != selected.end(); ++it)
+            (*it)->unselect();
+        selected.clear();
+    }
 
-	if ((last_click_x == x) && (last_click_y == y)) { // Single selection
-		/* TODO: implement more sophisticated selection detection 
-		 		 (grid, quad-tree, etc) */
-		for (auto it = units.rbegin(); it != units.rend(); ++it) {
-			if ((*it)->is_clicked(x, y)) {
-				selected.insert((*it));
-				(*it)->select();
-				cerr << "selected\n";
-				return;
-			}
-		}
+    if ((last_click_x == x) && (last_click_y == y)) { // Single selection
+        /* TODO: implement more sophisticated selection detection 
+                 (grid, quad-tree, etc) */
+        for (auto it = units.rbegin(); it != units.rend(); ++it) {
+            if ((*it)->is_clicked(x, y)) {
+                selected.insert((*it));
+                (*it)->select();
+                cerr << "selected\n";
+                return;
+            }
+        }
 
-		for (auto it = units.rbegin(); it != units.rend(); ++it) {
-			if ((*it)->is_broadly_clicked(x, y)) {
-				selected.insert((*it));
-				(*it)->select();
-				cerr << "broadly selected\n";
-				return;
-			}
-		}
-	} else { // Range selection
-		for (auto it = units.rbegin(); it != units.rend(); ++it) {
-			if ((*it)->intersects(rect)) {
-				selected.insert((*it));
-				(*it)->select();
-				cerr << "range selected\n";
-			}
-		}
-	}
+        for (auto it = units.rbegin(); it != units.rend(); ++it) {
+            if ((*it)->is_broadly_clicked(x, y)) {
+                selected.insert((*it));
+                (*it)->select();
+                cerr << "broadly selected\n";
+                return;
+            }
+        }
+    } else { // Range selection
+        for (auto it = units.rbegin(); it != units.rend(); ++it) {
+            if ((*it)->intersects(rect)) {
+                selected.insert((*it));
+                (*it)->select();
+                cerr << "range selected\n";
+            }
+        }
+    }
 }
 
 void unitManager::mouse_move(int x, int y) {
-	if (clicked) {
-		rect.set_values(MIN(last_click_y, y), 
-						MAX(last_click_y, y), 
-						MIN(last_click_x, x), 
-						MAX(last_click_x, x));
-	}
+    if (clicked) {
+        rect.set_values(MIN(last_click_y, y), 
+                        MAX(last_click_y, y), 
+                        MIN(last_click_x, x), 
+                        MAX(last_click_x, x));
+    }
 }
 
 void unitManager::tick() {
-	if (clicked) {
-		rect.clear();
-	}
-	for (auto it = units.rbegin(); it != units.rend(); ++it) {
-		(*it)->clear();
-	}
-	units.resort();
-	for (auto it = units.begin(); it != units.end(); ++it) {
-		(*it)->draw();
-	}
-	if (clicked) {
-		rect.draw();
-	}
+    if (clicked) {
+        rect.clear();
+    }
+    for (auto it = units.rbegin(); it != units.rend(); ++it) {
+        (*it)->clear();
+    }
+    units.resort();
+    for (auto it = units.begin(); it != units.end(); ++it) {
+        (*it)->draw();
+    }
+    if (clicked) {
+        rect.draw();
+    }
 }
