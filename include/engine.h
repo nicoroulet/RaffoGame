2ed61d8743abc9af47a113aed9a54316d2284a4f
nico <nicoroulet@gmail.com>
2017-08-13 03:51:56 -0300

    Basic Engine

---
+++ include/engine.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "camera.h"
+#include "map.h"
+#include "ship.h"
+#include "unit.h"
+
+#include <allegro5/allegro.h>
+
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_color.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_primitives.h>
+
+class Engine {
+public:
+    Engine(int fps, ALLEGRO_DISPLAY *display);
+    void start();
+    void loop();
+    void stop();
+private:
+    int fps;
+    int window_width;
+    int window_height;
+    ALLEGRO_DISPLAY *display;
+
+    bool shift;
+    bool lctrl;
+    bool click;
+    bool turning_right;
+    bool turning_left;
+
+    const static float ZOOM_LIM_OUT;
+    const static float ZOOM_LIM_IN;
+
+    ALLEGRO_EVENT_QUEUE *events;
+    ALLEGRO_EVENT_SOURCE *mouse;
+    ALLEGRO_TIMER *timer;
+    ALLEGRO_EVENT_SOURCE *keyboard;
+
+    Camera camera;
+    CameraShot shot;
+};
