41beca3c5fcd266181d6022b4788cfb6c184a6d6
Gaston Fourcade <gaston.fdm@gmail.com>
2017-08-14 16:27:18 -0300

    Better turn_left and right

--- src/ship.cpp
+++ src/ship.cpp
@@ -25,7 +25,7 @@ void Ship::draw(Camera &camera) {
     move();
     camera.set_transform_ship();
 
-    drawable->draw(- drawable->get_width() / 2, - drawable->get_height() / 2);
+    drawable->draw(-drawable->get_width() / 2, -drawable->get_height() / 2);
     for (auto &unit : crew) {
         unit->draw();
     }
@@ -37,7 +37,7 @@ void Ship::move() {
     This calculation could change depending type of sails */
     int w_speed = Weather::wind_speed;
     radians w_direction = Weather::wind_direction;
-    float push = (w_speed-speed) * (cos(w_direction-rotation)+1)/2;
+    float push = (w_speed - speed) * (cos(w_direction - rotation) + 1) / 2;
     /* Hull drag could be a Ship modifier */
     float drag = 0.05;
     /* Sails could be a Ship Modifier 
@@ -46,20 +46,28 @@ void Ship::move() {
     float sails_type = 0.8;
     float sails_change_rate = 0.1;
     /* Sails calculation */
-    sails_efficiency += (sails_aperture - sails_efficiency)*sails_change_rate;
+    sails_efficiency += (sails_aperture - sails_efficiency) * sails_change_rate;
     /* New speed with inertia */
     float acceleration = 0.02;
-    goal_speed = (1-drag)*sails_type*(speed + push*sails_efficiency);
+    goal_speed = (1 - drag) * sails_type * (speed + push * sails_efficiency);
     speed += (goal_speed-speed)*acceleration;
 
     x -= speed * sin(rotation);
     y -= speed * cos(rotation);
 }
 
-void Ship::turn(float rotate, int direction) {
+void Ship::turn_left() {
+    turn(0.01);
+}
+
+void Ship::turn_right() {
+    turn(-0.01);
+}
+
+void Ship::turn(float rotate) {
     /* handling could be a Ship modifier */
     float handling = 0.9;
-    rotation += direction*rotate*handling*(0.1*speed);
+    rotation += rotate * handling * (0.1 * speed);
     /* Rotation penalty could be a Ship Modifier */
     float turn_penalty = 0.99;
     speed *= turn_penalty;
