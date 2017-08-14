59ba4d1b79916f18be78264d1770bbbd0a503332
Gaston Fourcade <gaston.fdm@gmail.com>
2017-08-14 17:21:30 -0300

    zoom limits in camera

--- include/camera.h
+++ include/camera.h
@@ -19,10 +19,10 @@ public:
     //void set_center(Ship *ship);
     int get_pos_x();
     int get_pos_y();
-    void set_zoom(float scale);
     void set_position(int x, int y, float rotate);
     void set_transform_ship();
     void set_transform_map();
+    void change_zoom(float factor);
     void change_shot();
 
 private:
@@ -35,6 +35,9 @@ private:
     int screen_height;
     CameraShot shot;
     ALLEGRO_TRANSFORM transform;
+
+    const static float ZOOM_LIM_OUT;
+    const static float ZOOM_LIM_IN;
 };
 
 
